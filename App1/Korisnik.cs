7b4bb7573e3f004475dacc03077a62351bf20574
domac <domac@DESKTOP-9CPPBVB>
2022-05-17 11:22:43 +0200

    Add project files.

---
+++ App1/Korisnik.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    internal class Korisnik {
+        public string ID { get; set; }
+        public string ime { get; set; }
+        public string prezime { get; set; }
+        public double visina { get; set; }
+        public double tezina { get; set; }
+    }
+}
